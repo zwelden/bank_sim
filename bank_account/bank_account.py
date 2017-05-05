0cd52a8fd0a7ee67f0ffb6a2bf033bbbd69500ab
zwelden <zach.cw@gmail.com>
2017-05-04 21:06:37 -0500

    added new methods to bank_account.py and updated tests.py

--- bank_account/bank_account.py
+++ bank_account/bank_account.py
@@ -1,15 +1,21 @@
 class BankAccount:
 
-    def __init__(self, initial_dep=0, f_name="", l_name="", phone="", address=""):
+    def __init__(self, initial_dep=0, f_name="", l_name="", phone="", address="", account_no=0):
         self.balance = initial_dep
         self.first_name = f_name
         self.last_name = l_name
         self.phone_number = phone
         self.address = address
+        self.account_number = account_no
+        self.transactions = 0
+        self.transactions_record = {}
 
     def deposit(self, dep_amount):
         if dep_amount > 0:
             self.balance += dep_amount
+            self.transactions += 1
+            self.transactions_record[self.transactions] = ("D", dep_amount)
+
         else:
             raise ValueError("deposit amount must be greater than 0")
 
@@ -17,6 +23,8 @@ class BankAccount:
         if wd_amount > 0:
             if wd_amount <= self.balance:
                 self.balance -= wd_amount
+                self.transactions += 1
+                self.transactions_record[self.transactions] = ("W", wd_amount)
             else:
                 raise ValueError("The amount to withdrawl is greater than the current ballance")
         else:
@@ -24,3 +32,49 @@ class BankAccount:
 
     def get_balance(self):
         return self.balance
+
+    def set_account_number(self, account_no):
+        if self.account_number == 0:
+            self.account_number = account_no
+        else:
+            raise AttributeError("Bank Account already has an account number assined")
+
+    def get_account_number(self):
+        return self.account_number
+
+    def set_account_holder_info(self, f_name, l_name, phone_num, address):
+        self.first_name = f_name
+        self.last_name = l_name
+        self.phone_number = phone_num
+        self.address = address
+
+    def get_account_holder_info(self):
+        account_num = "Account Number: {0}".format(self.account_number)
+        name = "Name: {0} {1}".format(self.first_name, self.last_name)
+        ph_num = "Phone Number: {0}".format(self.phone_number)
+        address = "Address: {0}".format(self.address)
+        account_holder_info = "{0}\n{1}\n{2}\n{3}".format(account_num, name, ph_num, address)
+        return account_holder_info
+
+    def get_account_status(self):
+        account_num = "Account Number: {0}".format(self.account_number)
+        bal = "Balance: {0}".format(self.balance)
+
+    def get_transactions(self):
+        if self.transactions > 0:
+            transactions_keys = list(self.transactions_record.keys())
+            transactions_keys.sort()
+            transactions_string = ""
+            if self.transactions < 10:
+                for key in transactions_keys:
+                    tran_type = self.transactions_record[key][0]
+                    amount = self.transactions_record[key][1]
+                    transactions_string += "Transaction No.: {0}  Type: {1}   Amount: {2}\n".format(key, tran_type, amount)
+            else:
+                for i in range(1, 11):
+                    tran_type = self.transactions_record[i][0]
+                    amount = self.transactions_record[i][1]
+                    transactions_string += "Transaction No.: {0}  Type: {1}   Amount: {2}\n".format(key, tran_type, amount)
+            return transactions_string
+        else:
+            return "No transactions to report on account"
--- test/test_bank_account.py
+++ test/test_bank_account.py
@@ -37,3 +37,53 @@ class TestBankAccount(unittest.TestCase):
         bank_account.deposit(25.75)
         bank_account.withdrawl(5.24)
         self.assertEqual(bank_account.get_balance(), 120.51)
+
+    def test_set_account_holder_info_on_new_account(self):
+        new_account = BankAccount(0)
+        new_account.set_account_holder_info("Zach", "Welden", "918-555-1234", "123 S Main St, Tulsa, OK, 74123")
+        self.assertTrue(new_account.first_name == "Zach")
+        self.assertTrue(new_account.last_name == "Welden")
+        self.assertTrue(new_account.phone_number == "918-555-1234")
+        self.assertTrue(new_account.address == "123 S Main St, Tulsa, OK, 74123")
+
+    def test_get_account_holder_info_after_creating_new_account(self):
+        new_account = BankAccount()
+        test_account_output_string = "Account Number: 0\nName:  \nPhone Number: \nAddress: "
+        self.assertTrue(new_account.get_account_holder_info() == test_account_output_string)
+
+    def test_get_account_holder_info_after_updating_account_number(self):
+        new_account = BankAccount()
+        new_account.set_account_number(123456789)
+        test_account_output_string = "Account Number: 123456789\nName:  \nPhone Number: \nAddress: "
+        self.assertTrue(new_account.get_account_holder_info() == test_account_output_string)
+
+    def test_get_account_holder_info_after_updating_account_number_and_account_holder_info(self):
+        new_account = BankAccount()
+        new_account.set_account_number(123456789)
+        new_account.set_account_holder_info("Zach", "Welden", "918-555-1234", "123 S Main St, Tulsa, OK, 74123")
+        test_account_output_string = "Account Number: 123456789\nName: Zach Welden\nPhone Number: 918-555-1234\nAddress: 123 S Main St, Tulsa, OK, 74123"
+
+    def test_transactions_report_on_new_account(self):
+        new_account = BankAccount()
+        self.assertTrue(new_account.get_transactions() == "No transactions to report on account")
+
+    def test_transaction_report_after_8_transactions_dep_and_wd(self):
+        new_account = BankAccount()
+        new_account.deposit(100)
+        new_account.withdrawl(25)
+        new_account.deposit(50)
+        new_account.withdrawl(75)
+        new_account.withdrawl(10)
+        new_account.deposit(20)
+        new_account.deposit(9)
+        new_account.withdrawl(17)
+        tsl1 = "Transaction No.: 1  Type: D   Amount: 100\n"
+        tsl2 = "Transaction No.: 2  Type: W   Amount: 25\n"
+        tsl3 = "Transaction No.: 3  Type: D   Amount: 50\n"
+        tsl4 = "Transaction No.: 4  Type: W   Amount: 75\n"
+        tsl5 = "Transaction No.: 5  Type: W   Amount: 10\n"
+        tsl6 = "Transaction No.: 6  Type: D   Amount: 20\n"
+        tsl7 = "Transaction No.: 7  Type: D   Amount: 9\n"
+        tsl8 = "Transaction No.: 8  Type: W   Amount: 17\n"
+        trans_string = tsl1 + tsl2 + tsl3 + tsl4 + tsl5 + tsl6 + tsl7 + tsl8
+        self.assertTrue(new_account.get_transactions() == trans_string)
